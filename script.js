69c584cb622bb4153384bca53a0c499c7c3dc22f
Hirendra77 <119104906+Hirendra77@users.noreply.github.com>
2023-02-05 14:23:11 +0530

    first

--- script.js
+++ script.js
@@ -14,37 +14,53 @@ let div= document.getElementById("userData");
 // thirdImage.disabled=true;
 
 // part first on click first image it will show form
+let arr1 =[0,0,0,0]
 firstImage.addEventListener("click", registration)
 function registration() {
-   document.getElementById('formGroup').classList.remove('hidden')
+    // if(arr.length===0) 
+    if(arr1[0]===0){
+   document.getElementById('formGroup').classList.remove('hidden')}
+   arr1[0]=1;
 }
 
 addBtn1.addEventListener("click", add)
 
 let arr=[];
-function add() {
-    document.getElementById('formGroup').classList.add('hidden')
-    let name = name1.value;
-    let email = email1.value;
-    let username = username1.value;
-    let obj1={
-        Name:name,
-        Email:email,
-        Username:username,
+function add(e) {
+   
+    e.preventDefault()
+        document.getElementById('formGroup').classList.add('hidden')
+        let name = name1.value;
+        let email = email1.value;
+        let username = username1.value;
+        let obj1={
+            Name:name,
+            Email:email,
+            Username:username,
+        }
+        
+        arr.push(obj1);
+        console.log(arr)
+        // firstImage.disabled=true;
     }
-    arr.push(obj1);
-    firstImage.disabled=true;
-}
- 
+    
+        
+    
+   
+
+
 
   //part 2 - on clicking second image it will display user data 
 secondImage.addEventListener('click',data)
 
 function data(){
+    if(arr1[1]===0){
     document.getElementById('formGroup').classList.add('hidden')
       document.getElementById('userData').classList.remove('hidden')
-   
-      div.innerHTML=`"Name"+obj1.Name+"Username"+obj1.Username ` 
+      arr1[1]=1;
+    }
+   console.log(arr);
+      div.innerHTML="Name:" +arr[0].Name +", "+ "Username:" +arr[0].Username;
     
     }
   
@@ -54,8 +70,11 @@ let imageDice = document.getElementById('diceImg')
 
 thirdImage.addEventListener('click',dice)
  function dice(){
+    if(arr1[2]===0){
     div.classList.add("hidden")
     imageDice.classList.remove("hidden")
+    arr1[2]=1;
+    }
  }
 
 
@@ -65,7 +84,6 @@ thirdImage.addEventListener('click',dice)
  let count = 0;
  let randomsum=0;
 function diceG(){
-   
     dicegame.classList.remove('hidden');
     count++;
     let arr=["1","2","3","4","5","6"]
@@ -73,22 +91,26 @@ function diceG(){
    
      if(count<=3){   
      //to generate random number between 0 to 5 but not included 6 only integer
-     let randomIndex= Math.floor(Math.random()*6)
+     let randomIndex= Math.floor(Math.random()*6+1)
      let randomDice=arr[randomIndex]
      randomsum+=parseInt(randomDice)
      span.innerText= randomsum; 
+    
      }
-
 }
 
+//part 4- Generate Token
 
 fourthImage.addEventListener('click', generatecoupan)
 let coupan =document.getElementById("coupan");
 let congratulations = document.getElementById('congrates')
 
 function generatecoupan() {
+    if(arr1[3]===0){
     imageDice.classList.add("hidden");
     dicegame.classList.add('hidden');
+    arr1[3]=1;
+    
     let letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let number = "0123456789"
     let digit = number + letter;
@@ -98,8 +120,11 @@ function generatecoupan() {
         token = token+ digit[random] 
    
     }
+    console.log(arr1)
     coupan.innerText=token;
     congratulations.classList.remove('hidden')
+    //  arr1[3]=1;
+    }
 }
 
 
@@ -119,15 +144,3 @@ function generatecoupan() {
 
 
 
-//       let table = document.createElement('table')
-//       let tr = document.createElement('tr')
-//       table.appendChild(tr);
-//       tr=table.insertRow(-1);
-//        for(let t of arr){
-//         for(let key in t){
-//             td = document.createElement("td")
-//             tr.appendChild(td);
-//             td.innerText = `${key}:${t[key]}`
-//         }
-//        }
-//   document.div.appendChild(table);
